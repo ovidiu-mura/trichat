4ee58d35fcd1d0865e888759a570563dae6be345
ad35@pdx.edu <ad35@augusta.cs.pdx.edu>
2019-08-10 17:25:37 -0700

    added some documentation

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 
 static const char *server_name = ">>server**";
 
+// For initial server connection
 typedef struct connection_info
 {
   int sockfd;
@@ -29,17 +30,17 @@ typedef struct pthread_arg_t
   int fd;
 } pthread_arg_t;
 
+// To create array of clients with relevant info
 typedef struct client_info {
   int fd;
   char *name;
   int online;
-  struct sockaddr_in addr; // ADDED
+  struct sockaddr_in addr; 
 }client_info;
 
-//ADDED
+// Holds data to be transmitted in I/O threads
 typedef struct client_data {
   int fd;
-  unsigned int size;
   struct data_pkt *pkt;
   struct init_pkt *init;
 }client_data;
@@ -57,7 +58,7 @@ epoll_task *to_read = NULL, *read_tail = NULL;
 epoll_task *to_write = NULL, *write_tail = NULL;
 pthread_t read_thr, write_thr, connect_thr;
 pthread_cond_t read_cond, write_cond;
-pthread_mutex_t read_lock, write_lock;//ADDED
+pthread_mutex_t read_lock, write_lock, lock, msg_lock;
 
 int sockfd, newSocket, num_users;
 struct sockaddr_in serverAddr, newAddr;
@@ -67,7 +68,6 @@ pthread_arg_t* pthread_arg;
 pthread_t client_thr;
 socklen_t len;
 char **users;
-pthread_mutex_t lock, msg_lock;
 struct client_info *clients;
 bool validate_input(char *a);
 
@@ -99,6 +99,7 @@ void write_queue_add(epoll_task *add)
   }
 }
 
+// Adds a new user, malloc'ing and populating relevant fields and incremented number of connected users
 int new_user(char *name, int fd)
 {
   if(pthread_mutex_trylock(&lock) != EBUSY){
@@ -137,6 +138,7 @@ int new_user(char *name, int fd)
     perror("malloc for new client name failed");
     return -1;
   }
+
   memcpy(clients[num_users].name, name, strlen(name));
   clients[num_users].fd = fd;
   clients[num_users].online = 1;
@@ -145,14 +147,13 @@ int new_user(char *name, int fd)
   return 0; 
 }
 
+// Broadcasts message to all online users
 int send_to_all(struct data_pkt *pkt)
 {
   if(pthread_mutex_trylock(&msg_lock) != EBUSY){
     printf("must be holding msg_lock\n");
     return -1;
   }
-
-  printf("send_to_all\n");
   int n;
   char msg[1024];
 
@@ -178,28 +179,12 @@ int send_to_all(struct data_pkt *pkt)
       ev.events = EPOLLIN | EPOLLET;
       if(epoll_ctl(epoll_fd, EPOLL_CTL_MOD, clients[i].fd, &ev) < 0)
         perror("epoll_ctl error: ");
-      printf("added %d to read list @183\n", clients[i].fd);
     }
   }
   return 0;
 } 
 
-int get_clientfd(char *user_to_find)
-{
-  if(!user_to_find)
-    return -1;
-
-  if(!strcmp(user_to_find, "ALL"))
-    return 0;
-
-  for(int i = 0; i < num_users; ++i){
-    if(!strncmp(user_to_find, clients[i].name, strlen(user_to_find)) && clients[i].online)
-      return clients[i].fd;
-  }
-
-  return -1;
-}
-
+// Sends a message to an individual client matching the send_to_fd argument
 int send_msg(struct data_pkt *pkt, int send_to_fd)
 {
   if(pthread_mutex_trylock(&msg_lock) != EBUSY){
@@ -207,7 +192,6 @@ int send_msg(struct data_pkt *pkt, int send_to_fd)
     return -1;
   }
 
-  printf("send_msg\n");
   int n;
   char msg[1024];
 
@@ -237,17 +221,79 @@ int send_msg(struct data_pkt *pkt, int send_to_fd)
           perror("epoll_ctl error: ");
           return -1;
         }
-        else{
-          printf("added %d to read list @241\n", send_to_fd);
-          return 0;
-        }
+        return 0;
       }
-      printf("error sending message\n");
+      printf("error sending message to %s\n", msg_pkt.dst);
       return -1;
     }
   }
   printf("user %s not found\n", pkt->dst);
   return -1;
+}
+
+// Sends a message directly from the server to a client
+int server_to_client_msg(int fd, struct init_pkt *pkt, char *msg)
+{
+  if(!p || !msg || fd < 0)
+    return -1;
+
+  struct data_pkt msg_pkt;
+  msg_pkt.type = DATA;
+  strcpy(msg_pkt.src, p->dst);
+  strcpy(msg_pkt.dst, p->src);
+  strcpy(msg_pkt.data, msg);
+  msg_pkt.id = 1;
+  char *u = ser_data(&msg_pkt, DATA);
+  char *user_data = hide_zeros((unsigned char*)u);
+  int n = send(fd, user_data, 1024, 0);
+
+  if(n > 0)
+    return 0;
+
+  if(n < 0){
+    perror("send error: ");
+    return -1;
+  }
+
+  if(!n){
+    close(fd);
+    printf("client closed connection\n");
+    return -1;
+  }
+  return -1;
+}
+
+// Returns the fd of the client whose username matches the argument
+int get_clientfd(char *user_to_find)
+{
+  if(!user_to_find)
+    return -1;
+
+  if(!strcmp(user_to_find, server_name))
+    return 0;
+
+  for(int i = 0; i < num_users; ++i){
+    if(!strncmp(user_to_find, clients[i].name, strlen(user_to_find)) && clients[i].online)
+      return clients[i].fd;
+  }
+
+  return -1;
+}
+
+int is_valid_fd(int fd)
+{
+  if(fd < 0){
+    printf("invalid fd %d\n", fd);
+    return 0;
+  }
+
+  for(int i = 0; i < num_users; ++i){
+    if(clients[i].fd == fd)
+      return fd;
+  }
+
+  printf("invalid fd %d\n", fd);
+  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -283,56 +329,12 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-int is_valid_fd(int fd)
-{
-  if(fd < 0){
-    printf("invalid fd %d\n", fd);
-    return 0;
-  }
-
-  for(int i = 0; i < num_users; ++i){
-    if(clients[i].fd == fd)
-      return fd;
-  }
-
-  printf("invalid fd %d\n", fd);
-  return 0;
-}
-
-int server_to_client_msg(int fd, struct init_pkt *pkt, char *msg)
-{
-  if(!p || !msg || fd < 0)
-    return -1;
-
-  struct data_pkt msg_pkt;
-  msg_pkt.type = DATA;
-  strcpy(msg_pkt.src, p->dst);
-  strcpy(msg_pkt.dst, p->src);
-  strcpy(msg_pkt.data, msg);
-  msg_pkt.id = 1;
-  char *u = ser_data(&msg_pkt, DATA);
-  char *user_data = hide_zeros((unsigned char*)u);
-  int n = send(fd, user_data, 1024, 0);
-
-  if(n > 0)
-    return 0;
-
-  if(n < 0){
-    perror("send error: ");
-    return -1;
-  }
-
-  if(!n){
-    close(fd);
-    printf("client closed connection\n");
-    return -1;
-  }
-  return -1;
-}
-
+/* Starting routine for connection thread:
+   Calls epoll_wait to identify fds ready for activity, then sets up necessary action */
 void* accept_conn(void *arg)
 {
   unsigned char *d = malloc(1024);
+  memset(d, '\0', 1024);
   char *u;
   int opt_arg, n, fd;
   connection_info *connection = (struct connection_info*)arg;
@@ -343,9 +345,8 @@ void* accept_conn(void *arg)
     if(!(ready = epoll_wait(epoll_fd, events, MAXUSERS, TIMEOUT)))
       continue;
 
-    printf("READY: %d\n", ready);
-
     for(int i = 0; i < ready; ++i){
+      // A client is trying to connect to the server socket
       if(events[i].data.fd == connection->sockfd) {
         len = sizeof(connection->serverAddr);
         fd = accept(connection->sockfd, (struct sockaddr*)&connection->serverAddr, &len);
@@ -357,7 +358,7 @@ void* accept_conn(void *arg)
         if(n < 0){
           if(errno == ECONNRESET)
             close(fd);
-          strerror(errno);
+          perror("error receiving init packet for new connection");
           continue;
         }
         else if(!n){
@@ -371,6 +372,7 @@ void* accept_conn(void *arg)
         d[n] = '\0';
         u = unhide_zeros(d);
 
+        // Init packet has been received, server sends back ack packet
         if(u[0] == 0x01){
           p = deser_init_pkt(u);
           pthread_mutex_lock(&lock);
@@ -393,11 +395,13 @@ void* accept_conn(void *arg)
           }
           char *users = get_user_list();
           if(server_to_client_msg(fd, p, users)){
+            free(users);
             pthread_mutex_unlock(&lock);
             printf("error sending message to client %s\n", p->src);
             continue;
           }
           int ret = new_user(p->src, fd);
+          free(users);
           pthread_mutex_unlock(&lock);
           if(!ret)
             printf("User %s has connected on fd %d\n", p->src, fd); 
@@ -405,9 +409,11 @@ void* accept_conn(void *arg)
             continue;
           // PROMPT FOR ANOTHER NAME
           else{
-            if(d) free(d);
+            memset(d, '\0', 1024);
             continue;
           }
+
+          // Set new client's fd as non-blocking
           opt_arg = fcntl(fd, F_GETFL);
           if(opt_arg < 0){
             perror("fcntl error with F_GETFL\n");
@@ -420,11 +426,11 @@ void* accept_conn(void *arg)
           }
           ev.data.fd = fd;
           ev.events = EPOLLIN | EPOLLET;
-          if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) < 0)
+          if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) < 0) // Add to interest list for reading:w
             perror("epoll_ctl error: ");
-          printf("added %d to read list @405\n", fd);
         }
-      } 
+      }
+      // fd is ready to be read from
       else if(events[i].events & EPOLLIN && is_valid_fd(events[i].data.fd)){
         epoll_task *task = malloc(sizeof(struct epoll_task));
         task->data.fd = events[i].data.fd;
@@ -434,6 +440,7 @@ void* accept_conn(void *arg)
         pthread_cond_broadcast(&read_cond);
         pthread_mutex_unlock(&read_lock);
       } 
+      // fd is ready to be written to
       else if(events[i].events & EPOLLOUT){
         if(!events[i].data.ptr)
           continue;
@@ -450,7 +457,7 @@ void* accept_conn(void *arg)
     }
   }
 
-  printf("Server exiting...accept_conn\n");
+  printf("Server exiting...\n");
   return 0;
 }
 
@@ -459,13 +466,14 @@ void* do_reads(void *arg)
   client_data *cli_data = NULL;
   int n;
   unsigned char *d = malloc(1024);
+  memset(d, '\0', 1024);
   char *u;
   char data[1024];
 
   for(;;){
     pthread_mutex_lock(&read_lock);
     while(!to_read)
-      pthread_cond_wait(&read_cond, &read_lock);
+      pthread_cond_wait(&read_cond, &read_lock); // Wait until ready to read and woken by connect_thr
     int fd = to_read->data.fd;
     epoll_task *task = to_read;
     to_read = to_read->next;
@@ -477,13 +485,13 @@ void* do_reads(void *arg)
     if(n < 0){
       if(errno == ECONNRESET)
         close(fd);
-      strerror(errno);
+      printf("error reading from fd %d", fd);
       free(cli_data);
       continue;
     }
     else if(!n){
       close(fd);
-      printf("client closed connection\n");
+      printf("client on fd %d closed connection\n", fd);
       free(cli_data);
       continue;
     }
@@ -491,7 +499,6 @@ void* do_reads(void *arg)
     printf("bytes received %d\n", n);
     if(!strcmp(data, ":exit"))
     {
-      printf("exiting at 478\n");
       pthread_mutex_lock(&lock);
       for(int i = 0; i < num_users; ++i){
         if(!strcmp(cli_data->pkt->src, clients[i].name)){
@@ -502,16 +509,7 @@ void* do_reads(void *arg)
         }
       }
       pthread_mutex_unlock(&lock);
-      /*if(!ret) {
-        printf("User %s has connected\n", p->src); 
-        send(thr_sockfd, udata, strlen(udata), 0);
-      } else if(ret == 1)
-        ;
-      // PROMPT FOR ANOTHER NAME
-      else
-        return thr_cleanup((char*)d, arg, thr_sockfd);
-    }
-    if(u[0] == 0x03)
+   /* if(u[0] == 0x03)
     {
       struct ack_pkt ack;
       ack.type = ACK;
@@ -532,7 +530,7 @@ void* do_reads(void *arg)
         pthread_mutex_unlock(&msg_lock);
       }
     } else if(u[0] == 0x4) {
-//      printf("CLS packet received.\n");
+      printf("CLS packet received.\n");
       printf("Disconnected %s:%d\n", inet_ntoa(thr_addr.sin_addr), ntohs(thr_addr.sin_port));
       break;
     } else
@@ -546,25 +544,21 @@ void* do_reads(void *arg)
     d[n] = '\0';
     u = unhide_zeros(d);
 
-    if(u[0] == 0x01){
-      printf("init packet received in do_reads\n");
+    if(u[0] == 0x01){ // Already received init packet in connect_thr, shouldn't receive another
       free(cli_data);
       continue;
     }
 
-    if(u[0] == 0x03)
+    if(u[0] == 0x03) // Received msg from client
     {
-      printf("data packet received in do_reads\n");
-      cli_data->size = n;
       cli_data->pkt = (struct data_pkt*)deser_data_pkt(u);
       if(!strcmp(cli_data->pkt->data, ":exit"))
       {
-        printf("exiting at 509\n");
         pthread_mutex_lock(&lock);
         for(int i = 0; i < num_users; ++i){
           if(!strcmp(cli_data->pkt->src, clients[i].name)){
             clients[i].online = 0;
-            printf("Disconnected %s\n", inet_ntoa(clients[i].addr.sin_addr));
+            printf("Disconnected %s:%d\n", inet_ntoa(clients[i].addr.sin_addr), ntohs(clients[i].addr.sin_port));
             --num_users;
             break;
           }
@@ -574,34 +568,31 @@ void* do_reads(void *arg)
         continue;
       }
       if(strcmp(cli_data->pkt->dst, server_name)){ // Message to a particular client
-        printf("msg @524: %s\n", cli_data->pkt->data);
         cli_data->fd = get_clientfd(cli_data->pkt->dst);
         if(fd == -1){
           printf("User %s not available", cli_data->pkt->dst);
           free(cli_data);
           continue;
         }
-        ev.data.ptr = cli_data;
+        ev.data.ptr = cli_data; // Contains message packet
         ev.events = EPOLLOUT | EPOLLET;
-        if(epoll_ctl(epoll_fd, EPOLL_CTL_MOD, cli_data->fd, &ev) < 0)
+        if(epoll_ctl(epoll_fd, EPOLL_CTL_MOD, cli_data->fd, &ev) < 0) // Change fd for destination client from read list to write list
           perror("epoll_ctl error:");
-        printf("added %d to write list @522\n", cli_data->fd);
       }
       else {   // Message to all
-        printf("msg @538: %s\n", cli_data->pkt->data);
         ev.data.ptr = cli_data;
         ev.events = EPOLLOUT | EPOLLET;
         for(int i = 0; i < num_users; ++i){
           if(clients[i].online && !strcmp(clients[i].name, cli_data->pkt->src)) {
             if(epoll_ctl(epoll_fd, EPOLL_CTL_MOD, clients[i].fd, &ev) < 0)
               perror("epoll_ctl error:");
-            printf("added %d to write list @531\n", clients[i].fd);
           }
         }
       }
     }
-//    bzero(data, sizeof(data));
+    bzero(data, sizeof(data));
   }
+  if(d) free(d);
 }
 
 void* do_writes(void *arg)
@@ -623,18 +614,18 @@ void* do_writes(void *arg)
     pthread_mutex_lock(&msg_lock);
     printf("msg: %s\n", cli_data->pkt->data);
     if(!strcmp(cli_data->pkt->dst, server_name)){
-      printf("broadcasting msg\n");
       if(send_to_all(cli_data->pkt))
         printf("error broadcasting message\n");
       pthread_mutex_unlock(&msg_lock);
+      free(cli_data); // Don't need after message successfully sent
       continue;
     }
 
     fd = get_clientfd(cli_data->pkt->dst);
-    printf("fd to send to: %d\n", fd);
     if(send_msg(cli_data->pkt, fd)) 
       printf("error sending msg %s from %s to %s\n", cli_data->pkt->data, cli_data->pkt->src, cli_data->pkt->dst);
     pthread_mutex_unlock(&msg_lock);
+    free(cli_data);
   }
   perror("Server msg send error");
   //Cleanup thread
@@ -662,9 +653,9 @@ void startup(connection_info * connection,int port)
   }
   printf("[+]Server Socket is created.\n");
 
-  //ADDED
   int arg;
-  epoll_fd = epoll_create(MAXUSERS*2+1);
+  epoll_fd = epoll_create(MAXUSERS*2+1); // Create epoll instance and making server socket fd non-blocking
+
   if((arg = fcntl(connection->sockfd, F_GETFL)) < 0){
     perror("fcntl error with F_GETFL");
     exit(EXIT_FAILURE);
@@ -678,7 +669,7 @@ void startup(connection_info * connection,int port)
 
   ev.data.fd = connection->sockfd;
   ev.events = EPOLLIN | EPOLLET;
-  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connection->sockfd, &ev);//ADDED
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connection->sockfd, &ev);
 
   connection->serverAddr.sin_family = AF_INET;
   connection->serverAddr.sin_port = htons(port);
@@ -715,9 +706,10 @@ void* thr_cleanup(char *d, pthread_arg_t *arg, int fd)
   return 0;
 }
 
+// Sends user list to user user on connecting and when requested
 char* get_user_list()
 {
-  int num = 0;
+  int num = 1;
   char *user_list = malloc(1024);
   memset(user_list, '\0', 1024);
 
@@ -739,7 +731,7 @@ char* get_user_list()
 bool validate_input(char *a)
 {
   int i=0;
-  if (a[i] == '-') i=1;//negative number	
+  if (a[i] == '-') i=1; //negative number	
   for(;a[i]!='\0';i++)
   {
     if(!isdigit(a[i]))
